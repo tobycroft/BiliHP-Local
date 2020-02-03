7af88c14e617176582d7d3619e69dde6652e88ae
Tuuz <2539@aerofsx.com>
2020-02-03 22:26:58 +0800

    tuuz

---
+++ Conf/Conf.go
@@ -0,0 +1,33 @@
+package Conf
+
+import (
+	"github.com/Unknwon/goconfig"
+)
+
+func LoadConf(section string, key string) string {
+	cfg, err := goconfig.LoadConfigFile("conf.ini")
+	if err != nil {
+		panic(err)
+	}
+
+	value, err := cfg.GetValue("user", "username")
+	if err != nil {
+		return ""
+	} else {
+		return value
+	}
+}
+
+func SaveConf(section string, key string, value string) bool {
+	cfg, err := goconfig.LoadConfigFile("../conf.ini")
+	if err != nil {
+		return false
+	}
+	cfg.SetValue(section, key, value)
+	err = goconfig.SaveConfigFile(cfg, "conf.ini")
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
+}
--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/Unknwon/goconfig"
+	"main.go/Conf"
 )
 
 func main() {
-	cfg, err := goconfig.LoadConfigFile("conf.ini")
-	if err != nil {
-		panic("错误")
-	}
+	fmt.Println(Conf.LoadConf("user", "username"))
 
-	value, err := cfg.GetValue("user", "username")
-	fmt.Println(value)
-	cfg.SetValue("user", "username", "dda")
-	goconfig.SaveConfigFile(cfg, "conf.ini")
 }
