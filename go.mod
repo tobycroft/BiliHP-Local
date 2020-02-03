694bcdc10ceaf822f40a4e6aaf57c9c9c4abd726
Tuuz <2539@aerofsx.com>
2020-02-03 22:14:19 +0800

    tuuz

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main.go
 
 go 1.13
+
+require github.com/Unknwon/goconfig v0.0.0-20191126170842-860a72fb44fd // indirect
--- main.go
+++ main.go
@@ -1,84 +1,18 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"reflect"
-	"strings"
+	"github.com/Unknwon/goconfig"
 )
 
 func main() {
-
-	fmt.Println(GetConfig())
-	//conn, err := net.Dial("tcp", "10.0.0.181:81")
-	//defer conn.Close()
-	//if err != nil {
-	//	fmt.Printf("connect failed, err : %v\n", err.Error())
-	//	return
-	//}
-	//
-	//inputReader := bufio.NewReader(os.Stdin)
-	//
-	//for {
-	//	input, err := inputReader.ReadString('\n')
-	//	if err != nil {
-	//		fmt.Printf("read from console failed, err: %v\n", err)
-	//		break
-	//	}
-	//	trimmedInput := strings.TrimSpace(input)
-	//	if trimmedInput == "Q" {
-	//		break
-	//	}
-	//	_, err = conn.Write([]byte(trimmedInput))
-	//
-	//	if err != nil {
-	//		fmt.Printf("write failed , err : %v\n", err)
-	//		break
-	//	}
-	//}
-}
-
-type Config struct {
-	Username string
-	Token    string
-}
-
-func GetConfig() Config {
-	//创建一个空的结构体,将本地文件读取的信息放入
-	c := &Config{}
-	//创建一个结构体变量的反射
-	cr := reflect.ValueOf(c).Elem()
-	//打开文件io流
-	f, err := os.Open("./config.ini")
+	cfg, err := goconfig.LoadConfigFile("conf.ini")
 	if err != nil {
-		log.Fatal(err)
+		panic("错误")
 	}
-	defer func() {
-		if err = f.Close(); err != nil {
-			log.Fatal(err)
-		}
-	}()
-	//我们要逐行读取文件内容
-	s := bufio.NewScanner(f)
-	for s.Scan() {
-		//以=分割,前面为key,后面为value
-		var str = s.Text()
-		var index = strings.Index(str, "=")
-		var key = str[0:index]
-		var value = str[index+1:]
-		//因为Port是int,所以我们这里要将截取的string强转成int
 
-		//通过反射将字段设置进去
-		fmt.Println(key, value)
-		cr.FieldByName(key).Set(reflect.ValueOf(value))
-
-	}
-	err = s.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
-	//返回Config结构体变量
-	return *c
+	value, err := cfg.GetValue("user", "username")
+	fmt.Println(value)
+	cfg.SetValue("user", "username", "dda")
+	goconfig.SaveConfigFile(cfg, "conf.ini")
 }
