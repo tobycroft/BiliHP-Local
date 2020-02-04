0ef4d14af90a899627de5d6b2ecf059fbbc2d5d3
Tuuz <2539@aerofsx.com>
2020-02-04 16:01:43 +0800

    tuuz

--- main.go
+++ main.go
@@ -2,22 +2,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"main.go/Conf"
 	"main.go/Tcp"
 	"net/http"
+	"os/exec"
 )
 
 func main() {
 	username := Conf.LoadConf("user", "username")
 	token := Conf.LoadConf("user", "token")
+
 	fmt.Println(token)
 	if username != "" && token != "" {
 		Tcp.Create(username, token)
 	} else {
-
+		exec.Command(`cmd`, `/c`, `start`, `http://localhost/`).Start()
 	}
-	http.HandleFunc("/", HelloWorldHandler)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/writelogin", writelogin)
 	http.HandleFunc("/user/login", UserLoginHandler)
+	// 设置静态目录
+	fsh := http.FileServer(http.Dir("./html"))
+	http.Handle("/html/", http.StripPrefix("/html/", fsh))
+
 	err := http.ListenAndServe("0.0.0.0:80", nil)
 
 	if err != nil {
@@ -26,14 +35,51 @@ func main() {
 }
 
 func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("r.Method = ", r.Method)
-	fmt.Println("r.URL = ", r.URL)
-	fmt.Println("r.Header = ", r.Header)
-	fmt.Println("r.Body = ", r.Body)
-	fmt.Fprintf(w, "HelloWorld!")
+	//fmt.Println("r.Method = ", r.Method)
+	//fmt.Println("r.URL = ", r.URL)
+	//fmt.Println("r.Header = ", r.Header)
+	//fmt.Println("r.Body = ", r.Body)
+	//fmt.Fprintf(w, "HelloWorld!")
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	username := Conf.LoadConf("user", "username")
+	token := Conf.LoadConf("user", "token")
+	if username == "" || token == "" {
+		url := "/login"
+		http.Redirect(w, r, url, http.StatusFound)
+
+	} else {
+
+	}
+}
+
+func writelogin(response http.ResponseWriter, request *http.Request) {
+	fmt.Println("Handler Hello")
+	username := request.PostFormValue("username")
+	token := request.PostFormValue("token")
+	Conf.SaveConf("user", "username", username)
+	Conf.SaveConf("user", "token", token)
+	fmt.Fprintf(response, "Login Success")
 }
 
 func UserLoginHandler(response http.ResponseWriter, request *http.Request) {
 	fmt.Println("Handler Hello")
 	fmt.Fprintf(response, "Login Success")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	if r.Method == "GET" {
+		t, err := template.ParseFiles("html/login.html")
+		if err != nil {
+			fmt.Fprintf(w, "parse template error: %s", err.Error())
+			return
+		}
+		t.Execute(w, nil)
+	} else {
+		username := r.Form["username"]
+		password := r.Form["password"]
+		fmt.Fprintf(w, "username = %s, password = %s", username, password)
+	}
 }
