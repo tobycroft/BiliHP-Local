91f4e5e63630225c4523fb90a01ff028be4293ee
Tuuz <2539@aerofsx.com>
2020-02-22 15:46:28 +0800

    tuuz

--- tuuz/Jsong/Jsong.go
+++ tuuz/Jsong/Jsong.go
@@ -11,7 +11,7 @@ func Encode(data interface{}) (string, error) {
 	jb, err := json.Marshal(data)
 	//fmt.Println(jb)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("JENCODEEncode", err)
 		return "", err
 	}
 	return string(jb), err
@@ -52,39 +52,48 @@ func JToken(data string) (interface{}, error) {
 	return arr, err
 }
 
-func TCPJObject(data string) ([]map[string]interface{}, error) {
+func TCPJObject(temp *string) ([]map[string]interface{}, error) {
 	var arr []map[string]interface{}
+	var arr2 map[string]interface{}
 	var json = jsoniter.ConfigCompatibleWithStandardLibrary
 	//var strs []string
-	//fmt.Println(data)
+
+	data := *temp
 	strs := strings.Split(data, "}{")
 	if len(strs) > 1 {
 		for i, v := range strs {
 			if i == 0 {
-				v += "}"
+				strs[i] = v + "}"
 			} else if len(strs) == i+1 {
-				v = "{" + v
+
+				strs[i] = "{" + v
 			} else {
-				v = "{" + v + "}"
+				strs[i] = "{" + v + "}"
 			}
-			err := json.Unmarshal([]byte(v), &arr)
-			ret, err := JObject(v)
-			if err != nil {
-				return nil, err
-			} else {
-				arr = append(arr, ret)
-			}
+			//fmt.Println(strs[i])
 		}
-		return arr, nil
-	}
-	ret, err := JObject(data)
-	if err != nil {
+		data = "[" + strings.Join(strs, ",") + "]"
+		err := json.Unmarshal([]byte(data), &arr)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			//fmt.Println("ss:", arr)
+		}
+		*temp = ""
 		return arr, err
 	} else {
-		arr = append(arr, ret)
-		return arr, err
+		err := json.Unmarshal([]byte(data), &arr2)
+		if err != nil {
+			//fmt.Println("2",data)
+			//fmt.Println(err)
+			return nil, err
+		} else {
+			*temp = ""
+			arr = append(arr, arr2)
+			return arr, err
+		}
 	}
-	return arr, nil
+
 }
 
 func TCPJArray(data string) ([]interface{}, error) {
@@ -119,4 +128,46 @@ func TCPJArray(data string) ([]interface{}, error) {
 		return arr, err
 	}
 	return arr, err
+}
+
+func TCP_JSON_CUT(temp *string) (string, bool) {
+	var arr []map[string]interface{}
+	var arr2 map[string]interface{}
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	//var strs []string
+
+	data := *temp
+	strs := strings.Split(data, "}{")
+	if len(strs) > 1 {
+		for i, v := range strs {
+			if i == 0 {
+				strs[i] = v + "}"
+			} else if len(strs) == i+1 {
+
+				strs[i] = "{" + v
+			} else {
+				strs[i] = "{" + v + "}"
+			}
+			//fmt.Println(strs[i])
+		}
+		data = "[" + strings.Join(strs, ",") + "]"
+		err := json.Unmarshal([]byte(data), &arr)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			//fmt.Println("ss:", arr)
+		}
+		*temp = ""
+		return data, true
+	} else {
+		err := json.Unmarshal([]byte(data), &arr2)
+		if err != nil {
+			//fmt.Println("2",data)
+			//fmt.Println(err)
+			return "", false
+		} else {
+			*temp = ""
+			return data, true
+		}
+	}
 }
