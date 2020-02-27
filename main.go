5b02993fa2788d4212f31b0b8f1999c41ea08d03
TobyCroft <silverhawk@foxmail.com>
2020-02-27 20:58:06 +0800

    tuuz

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"main.go/tuuz/Net"
 	"net/http"
 	"os/exec"
-	"time"
 )
 
 func main() {
@@ -28,7 +27,8 @@ func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/panel", panel)
-	http.HandleFunc("/writelogin", writelogin)
+	//http.HandleFunc("/writelogin", writelogin)
+	http.HandleFunc("/logproc", logproc)
 	http.HandleFunc("/user/login", UserLoginHandler)
 	http.HandleFunc("/setting_get", setting_get)
 	http.HandleFunc("/setting_set", setting_set)
@@ -36,7 +36,7 @@ func main() {
 	fsh := http.FileServer(http.Dir("./html"))
 	http.Handle("/html/", http.StripPrefix("/html/", fsh))
 	fmt.Println("正在启动程序，请访问http://127.0.0.1")
-	time.Sleep(5 * time.Second)
+	//time.Sleep(5 * time.Second)
 	if username == "" || token == "" {
 		fmt.Println("你还没有登录，请访问上面的地址进行登录")
 	}
@@ -73,13 +73,115 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func logproc(w http.ResponseWriter, request *http.Request) {
-	login := "http://go.bilihp.com:180/v1/index/login/2"
+	login := "http://127.0.0.1/v1/index/login/2"
 	username := request.PostFormValue("username")
 	password := request.PostFormValue("password")
+	captcha := request.PostFormValue("captcha")
+	if len(captcha) < 4 {
+		ret := make(map[string]interface{})
+		ret["code"] = 0
+		ret["data"] = "验证码不能为空"
+		rrr, _ := Jsong.Encode(ret)
+		w.Write([]byte(rrr))
+		return
+	}
+
 	pm := make(map[string]interface{})
 	pm["username"] = username
 	pm["password"] = password
-	Net.PostCookie(login, nil, pm, nil, nil)
+	pm["captcha"] = captcha
+	req := Net.Request()
+	ret, err := req.Post(login, pm)
+	if err != nil {
+		return
+	}
+	body, err := ret.Body()
+	if err != nil {
+		return
+	}
+	//resp_header := ret.Headers()
+	//fmt.Println(cookie_arr)
+	if err != nil {
+		return
+	} else {
+
+		json, err := Jsong.JObject(string(body))
+		if err != nil {
+			fmt.Println("回传输出出错")
+			return
+		}
+
+		code := json["code"]
+		if code.(float64) == 0 {
+			data, err := Jsong.ParseObject(json["data"])
+			if err != nil {
+				fmt.Println("data数据错误")
+			}
+			header, err := Jsong.ParseObject2(data["header"])
+			if err != nil {
+				fmt.Println("header-数据错误")
+			}
+			cookie, err := Jsong.ParseObject2(data["cookie"])
+			if err != nil {
+				fmt.Println("cookie-数据错误")
+			}
+			values, err := Jsong.ParseObject(data["values"])
+			if err != nil {
+				fmt.Println("values-数据错误")
+			}
+			url := Calc.Any2String(data["url"])
+			req2 := Net.Request()
+			req2.SetHeaders(header)
+			req2.SetCookies(cookie)
+			ret, err := req2.Post(url, values)
+			if err != nil {
+				fmt.Println("ret-数据错误")
+				return
+			}
+			rtt, err := ret.Body()
+			if err != nil {
+				fmt.Println("rtt-数据错误")
+				return
+			}
+			arr := make(map[string]interface{})
+
+			arr["body"] = Jsong.Decode(string(rtt))
+
+			arr["statusCode"] = 200
+			req3 := Net.Request()
+			req3.SetHeaders(header)
+			req3.SetCookies(cookie)
+			cac := make(map[string]interface{})
+			cac["username"] = username
+			cac["password"] = password
+			cac["header"] = ret.Headers()
+			cac["ret"], _ = Jsong.Encode(arr)
+			ret3, err := req3.Post("http://127.0.0.1/v1/index/login/ret", cac)
+			b3, err := ret3.Body()
+			if err != nil {
+				fmt.Println("b3-数据错误")
+				return
+			}
+			ret4, err := Jsong.JObject(string(b3))
+			if err != nil {
+				fmt.Println("b4-最终数据错误")
+				return
+			} else {
+
+			}
+			fmt.Println(string(b3))
+		} else {
+			fmt.Println("数据错误")
+		}
+
+		//ret := make(map[string]interface{})
+		//ret["cookie"] = Net.CookieHandler(resp_header)
+		//ret["body"] = Jsong.Decode(string(body))
+		//ret["header"] = resp_header
+		//ret["statusCode"] = 200
+		//fmt.Println(Jsong.Encode(ret))
+
+	}
 }
 
 func writelogin(w http.ResponseWriter, request *http.Request) {
